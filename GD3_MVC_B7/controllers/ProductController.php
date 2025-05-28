a8ef89b346ce000293df84d2df60743041b30e76
thuyvt <thuyvt1005@gmail.com>
2025-05-28 11:35:28 +0700

    b7

---
+++ GD3_MVC_B7/controllers/ProductController.php
@@ -0,0 +1,50 @@
+<?php
+    class ProductController {
+        
+        // Khai báo thuộc
+        public $productQuery;
+        
+        public function __construct() {
+            $this->productQuery = new ProductQuery();
+        }
+        public function __destruct(){}
+
+        // Hàm list() xử lý trường hợp người dùng muốn vào trang danh sách
+        public function list() {
+            // Hiển thị view tương ứng
+            include "views/product/list.php";
+        }
+
+        // Hàm create() {}
+        public function create() {
+            // Hiển thị view tương ứng
+            include "views/product/create.php";
+        }
+
+        public function detail($id) {
+            if ($id !== "") {
+                // Hiển thị view tương ứng
+                include "views/product/detail.php";
+            } else {
+                echo "Lỗi: Không nhận được thông tin <hr>";
+            }
+        }
+        public function update($id) {
+            if ($id !== "") {
+                // Hiển thị view tương ứng
+                include "views/product/update.php";
+            } else {
+                echo "Lỗi: Không nhận được thông tin <hr>";
+            }
+        }
+
+        public function delete($id) {
+            if ($id !== "") {
+                // Hiển thị view tương ứng
+                echo "Xóa id: $id <hr>";
+            } else {
+                echo "Lỗi: Không nhận được thông tin <hr>";
+            }
+        }
+    }
+?>
---
+++ GD3_MVC_B7/index.php
@@ -0,0 +1,34 @@
+<?php
+    // 1. Nhúng tất cả các file trong model, controller
+    include "models/Product.php";
+    include "models/ProductQuery.php";
+    // include "models/Category.php";
+    // include "models/CategoryQuery.php";
+
+    include "controllers/ProductController.php";
+
+    // 2. Lấy giá trị cần thiết từ url
+
+    // Lấy giá trị act
+    $act = "";
+    if (isset($_GET["act"])) {
+        $act = $_GET["act"];
+    }
+    echo "Giá trị act là: $act <hr>";
+
+    // Lấy giá trị id trên url
+    $id = "";
+    if (isset($_GET["id"])) {
+        $id = $_GET["id"];
+    }
+    echo "Giá trị id là :$id<hr>";
+    // Điều hướng sang trang cần thiết
+    match($act) {
+        'product-list' => (new ProductController)->list() ,
+        'product-create' => (new ProductController)->create(),
+        'product-detail' => (new ProductController)->detail($id),
+        'product-update' => (new ProductController)->update($id),
+        'product-delete' => (new ProductController)->delete($id)
+    };
+    // echo "Đây là trang chủ";
+?>
